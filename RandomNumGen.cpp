ed98045e9a78ffdc287cf3de5a5d1b79432c2505
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2021-10-22 13:29:55 -0500

    Create RandomNumGen.cpp

---
+++ RandomNumGen.cpp
@@ -0,0 +1,15 @@
+/* Based on an algorithm example from https://www.freecodecamp.org/news/random-number-generator/
+1. Accept some initial input number, that is a seed or key.
+2. Apply that seed in a sequence of mathematical operations to generate the result. That result is the random number.
+3. Use that resulting random number as the seed for the next iteration.
+4. Repeat the process to emulate randomness.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
