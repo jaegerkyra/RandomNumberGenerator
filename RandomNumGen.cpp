4914c1077a406615e0052393b12893606b4db346
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2021-10-23 15:38:21 -0500

    Update RandomNumGen.cpp
    Started a random number generator (Seed is based on character length of word entered)

--- RandomNumGen.cpp
+++ RandomNumGen.cpp
@@ -6,10 +6,41 @@
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+string Input;
+
+void randNumGen() {
+	int x = Input.length();
+	//int y = alphabetNum();
+	x = (x * x) / 2;
+	cout << x << endl;
+	//cout << y << endl;
+
+}
+//Trying to work in a way to turn the first character of word into a number to be used for seed of random number generator
+/*void alphabetNum() {
+	char x = Input[0];
+	if (x == 'a') {
+		x = 1;
+	}
+	else if (x == 'b') {
+		x = 2;
+	}
+	else {
+		x = 3;
+	}
+}*/
 
 int main() {
 
+	cout << "This is a random number generator" << endl;
+	cout << "Enter a word to be used as a seed: " << endl;
+
+	cin >> Input;
+
+	randNumGen();
 
 	return 0;
 }
